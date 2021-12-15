1293d6ee7110eedfbff010348a0a86186e1c8b35
Bharath <56950806+bablubharath@users.noreply.github.com>
2021-12-15 14:01:31 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,24 +1,3 @@
-output "cognito_map" {
-  description = "cognito info"
-  value       = { "user_pool"        = aws_cognito_user_pool.user_pool.id
-    "identity_pool"    = aws_cognito_identity_pool.identity_pool.id
-    "auth_arn"         = aws_iam_role.authenticated.arn
-    "domain"           = "${aws_cognito_user_pool_domain.user_pool_domain.domain}.auth.${var.region}.amazoncognito.com"
-  }
-}
-
-output "user_pool_id" {
-  value = aws_cognito_user_pool.user_pool.id
-}
-
-output "identity_pool_id" {
-  value = aws_cognito_identity_pool.identity_pool.id
-}
-
-output "app_client_id" {
-  value = aws_cognito_user_pool_client.client.id
-}
-
 output "arn" {
   description = "Amazon Resource Name (ARN) of the domain"
   value       = join("", aws_elasticsearch_domain.es_domain.*.arn)
